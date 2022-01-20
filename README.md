76339ef37455553da7c1ce57e96b4f167ab1c08f
IsaacShelton <isaac.a.shelton@gmail.com>
2022-01-20 15:38:44 -0600

    Added more demo shapes

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # lyon_tessellation_svg_example
 Example of using lyon_tessellation to create a simple SVG
 
-![svg example output](https://raw.github.com/IsaacShelton/lyon_tessellation_svg_example/master/image.svg)
+![svg bezier](https://raw.github.com/IsaacShelton/lyon_tessellation_svg_example/master/svg_output/Bezier.svg)
+
+![svg circle](https://raw.github.com/IsaacShelton/lyon_tessellation_svg_example/master/svg_output/Circle.svg)
+
+![svg round rect](https://raw.github.com/IsaacShelton/lyon_tessellation_svg_example/master/svg_output/RoundRect.svg)
+
+![svg polygon](https://raw.github.com/IsaacShelton/lyon_tessellation_svg_example/master/svg_output/Polygon.svg)
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use lyon::math::point;
+#[macro_use]
+extern crate enum_display_derive;
+
+use lyon::path::{
+    builder::{BorderRadii, PathBuilder},
+    math::{point, Rect, Size},
+    Polygon, Winding,
+};
 use lyon::tessellation::{
     BuffersBuilder, FillOptions, FillTessellator, FillVertex, StrokeOptions, StrokeTessellator,
     StrokeVertex, VertexBuffers,
 };
 
+use std::fmt::Display;
 use svg::node::element::path::Data;
 use svg::node::element::Path;
 use svg::Document;
@@ -14,18 +22,69 @@ struct MyVertex {
     position: [f32; 2],
 }
 
+#[derive(Display)]
+enum Choice {
+    Bezier,
+    Circle,
+    RoundRect,
+    Polygon,
+}
+
 fn main() {
+    let choices = [
+        Choice::Bezier,
+        Choice::Circle,
+        Choice::RoundRect,
+        Choice::Polygon,
+    ];
+
+    for choice in choices {
+        make_example(choice);
+    }
+}
+
+fn make_example(choice: Choice) {
     let mut builder = lyon::path::Path::builder();
-    builder.begin(point(0.0, 0.0));
-    builder.quadratic_bezier_to(point(60.0, 0.0), point(90.0, 90.0));
-    builder.end(true);
 
+    // Build abstract geometry
+    match choice {
+        Choice::Bezier => {
+            builder.begin(point(0.0, 0.0));
+            builder.quadratic_bezier_to(point(60.0, 0.0), point(90.0, 90.0));
+            builder.end(true);
+        }
+        Choice::Circle => {
+            builder.add_circle(point(0.0, 0.0), 90.0, Winding::Positive);
+        }
+        Choice::RoundRect => {
+            builder.add_rounded_rectangle(
+                &Rect::new(point(5.0, 5.0), Size::new(80.0, 80.0)),
+                &BorderRadii::new(5.0),
+                Winding::Positive,
+            );
+        }
+        Choice::Polygon => {
+            let polygon = Polygon {
+                points: &[point(45.0, 10.0), point(80.0, 80.0), point(10.0, 80.0)],
+                closed: true,
+            };
+
+            builder.add_polygon(polygon);
+        }
+    }
+
+    // Create svg representation
     let path = builder.build();
-
     let document = stroke(&path);
     let document = document.add(fill(&path));
 
-    svg::save("image.svg", &document).unwrap();
+    // Create result directory
+    let result_directory = "svg_output";
+    std::fs::create_dir_all(result_directory).expect("Failed to create output directory");
+
+    // Save svg file
+    let filename = format!("{}/{}.svg", result_directory, choice);
+    svg::save(&filename, &document).unwrap();
 }
 
 fn stroke(lyon_path: &lyon::path::Path) -> svg::Document {
@@ -33,6 +92,7 @@ fn stroke(lyon_path: &lyon::path::Path) -> svg::Document {
     let mut geometry: VertexBuffers<MyVertex, u16> = VertexBuffers::new();
     let mut tessellator = StrokeTessellator::new();
 
+    // Create tessellated geometry for stroke
     tessellator
         .tessellate_path(
             lyon_path,
@@ -43,6 +103,7 @@ fn stroke(lyon_path: &lyon::path::Path) -> svg::Document {
         )
         .unwrap();
 
+    // Create svg representation for stroke geometry
     make_document(geometry, "blue")
 }
 
@@ -51,7 +112,7 @@ fn fill(lyon_path: &lyon::path::Path) -> svg::Document {
     let mut geometry: VertexBuffers<MyVertex, u16> = VertexBuffers::new();
     let mut tessellator = FillTessellator::new();
 
-    // Compute the tessellation.
+    // Create tessellated geometry for fill
     tessellator
         .tessellate_path(
             lyon_path,
@@ -62,12 +123,15 @@ fn fill(lyon_path: &lyon::path::Path) -> svg::Document {
         )
         .unwrap();
 
+    // Create svg representation for fill geometry
     make_document(geometry, "red")
 }
 
 fn make_document(geometry: VertexBuffers<MyVertex, u16>, fill_color: &str) -> svg::Document {
+    // Create empty svg document with view window 90x90
     let mut document = Document::new().set("viewBox", (0, 0, 90, 90));
 
+    // Add each triangle
     for triangle in 0..(geometry.indices.len() / 3) {
         let mut data = Data::new();
 
